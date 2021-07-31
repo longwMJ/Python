4d0c46e49e95429c805a37b84da0fb93693952c3
longwMJ <53077305+longwMJ@users.noreply.github.com>
2021-07-31 11:50:12 +0800

    初步访问网站

--- src/01.py
+++ src/01.py
@@ -1,5 +1,17 @@
 
 
-# 运行py文件: shift + 回车
+# 运行py文件: shift + 回车 , 或者点击右上角有三角形
 
-print("xue223 6")
+from urllib.request import urlopen
+
+url = 'https://blog.csdn.net/qq_42098115/article/details/106793000'
+
+resp = urlopen(url)
+
+# name = 'xue'
+
+# print(name)
+print(resp.read().decode("utf-8"))
+
+
+
