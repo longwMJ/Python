ba254efa655b9f73d85d3ecd17abbad68aa1d2bf
longwMJ <53077305+longwMJ@users.noreply.github.com>
2021-07-31 12:04:59 +0800

    保存网页

--- src/01.py
+++ src/01.py
@@ -5,6 +5,7 @@
 from urllib.request import urlopen
 
 url = 'https://blog.csdn.net/qq_42098115/article/details/106793000'
+# url = 'https://www.baidu.com/'
 
 resp = urlopen(url)
 
@@ -14,4 +15,15 @@ resp = urlopen(url)
 print(resp.read().decode("utf-8"))
 
 
+# 保存网页
+# with open("demo.html", mode="w") as f:
+#     f.write(resp.read().decode("utf-8"))
+#     print("over")
 
+# val = resp.read().decode("utf-8")
+
+with open('demo7.html', 'w') as f:
+    f.write(resp.read().decode("utf-8"))
+    # f.write(val)
+    # f.flush()  # 强制将缓存区内容写进文件
+    print("over ok")
