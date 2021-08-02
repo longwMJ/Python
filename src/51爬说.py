b57131e3e4e92ef47a2d7d4ec6ef5b23b4944dda
longwMJ <53077305+longwMJ@users.noreply.github.com>
2021-08-02 17:00:12 +0800

    feat: 添加进度条显示

--- src/51爬说.py
+++ src/51爬说.py
@@ -1,18 +1,13 @@
 import requests
 import parsel
-from bs4 import BeautifulSoup
+from tqdm import tqdm
+import time
 
 url = 'https://www.biquges.com/66_66497'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
 }
-
-# res = requests.get(url=url, headers=headers)
-
-
-# <Response [200]>
-# print(res)
 
 def save(name, title, content):
     with open(name + '.txt', mode='a', encoding='utf-8') as f:
@@ -35,7 +30,7 @@ def getRes(html_url):
 
     # print(hrefs)
 
-    for i in hrefs:
+    for i in tqdm(hrefs):
         link_url = 'https://www.biquges.com' + i
         childText = getChildRes(link_url).text
         # print(childText)
@@ -46,12 +41,11 @@ def getRes(html_url):
         childContentJoin = ''.join(childContentList)
         # print(childContentJoin)
         save(novel_title, childName, childContentJoin)
-
-        break
+        time.sleep(1)
+        # break
     
     res.close()
-
-
+    print('over')
 
 getRes(html_url=url)
 
