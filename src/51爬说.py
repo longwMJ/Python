ae763639f8bad4dbcb60fc85573e04d2f46876a9
longwMJ <53077305+longwMJ@users.noreply.github.com>
2021-08-02 16:44:13 +0800

    feat: save one text

--- src/51爬说.py
+++ src/51爬说.py
@@ -14,12 +14,42 @@ headers = {
 # <Response [200]>
 # print(res)
 
+def save(name, title, content):
+    with open(name + '.txt', mode='a', encoding='utf-8') as f:
+        f.write(title)
+        f.write('\n')
+        f.write(content)
+        f.write('\n')
+        f.close()
+
+def getChildRes(html_url):
+    res = requests.get(url=html_url, headers=headers)
+    return res
+
 def getRes(html_url):
     res = requests.get(url=html_url, headers=headers)
     selector = parsel.Selector(res.text)
     novel_title = selector.css('#info h1::text').get()
     print(novel_title)
+    hrefs = selector.css('#list dd a::attr(href)').getall()
 
+    # print(hrefs)
+
+    for i in hrefs:
+        link_url = 'https://www.biquges.com' + i
+        childText = getChildRes(link_url).text
+        # print(childText)
+        childSelector = parsel.Selector(childText)
+        childName = childSelector.css('.bookname h1::text').get()
+        childContentList = childSelector.css('#content::text').getall()
+        # 转字符串 强转  用''.join()
+        childContentJoin = ''.join(childContentList)
+        # print(childContentJoin)
+        save(novel_title, childName, childContentJoin)
+
+        break
+    
+    res.close()
 
 
 
